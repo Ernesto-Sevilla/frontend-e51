d7075c9ba376842b0541a484d6d2ef97499cd7d4
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2025-10-09 09:21:40 -0600

    Practicas con importaciones y exportaciones con componentes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import "./App.css";
+import Titulo from './components/Titulo.jsx';
+import Mensaje from './components/Mensaje.jsx';
 
 const App = () => {
   return (
     <>
-    <h1> Ferreteria Ernesto Sevilla</h1> 
+    <h1>Ferreteria Ernesto Sevilla</h1> 
+    <Titulo />
+    <Mensaje/>
     </>
   );
 };
 
+
 export default App;
+
---
+++ src/components/Mensaje.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Mensaje = () => {
+    return (
+        <>
+            <h1>Programar con React es genial por:</h1>
+            <ol>
+                <li>Su facilidad de uso</li>
+                <li>Por que lo puedo Chat GPtear</li>
+                <li>Porque si</li>
+            </ol>
+        </>
+    )
+};
+
+export default Mensaje;
